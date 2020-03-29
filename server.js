859c44d07a5e79175744615a2981b878ef38e8c7
zissan <zissan.chowdhury@yahoo.com>
2020-03-29 12:17:43 +0530

    Integrated socket.io.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+io.on("connection", socket => {
+  console.log("A USER IS CONNECTED");
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`SERVER LISTENING ON PORT ${PORT}`);
 });
